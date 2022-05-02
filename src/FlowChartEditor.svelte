ed9525d6f12d92e980d8ed5800f453cab1ea1ba6
Nathan Bean <g0122nb@genmills.com>
2022-05-02 11:11:54 -0500

    first commit

---
+++ src/FlowChartEditor.svelte
@@ -0,0 +1,17 @@
+<script>
+    import MachineNode from "./MachineNode.svelte";
+</script>
+
+<div class="flow-chart-editor">
+    <h1>Flow Chart Editor</h1>
+    <MachineNode />
+</div>
+
+<style>
+    .flow-chart-editor {
+        border: 3px solid #2fc910;
+        width: 100%;
+        height: 600px;
+        position: absolute;
+    }
+</style>
---
+++ src/MachineNode.svelte
@@ -0,0 +1,12 @@
+<div class="machine-node">
+    <h4>Machine Node</h4>
+</div>
+
+<style>
+    .machine-node {
+        background-color: #1c6ea4;
+        width: 160px;
+        height: 160px;
+        position: relative;
+    }
+</style>
